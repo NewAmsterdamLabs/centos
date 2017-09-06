3a403b47d509fc45808c3551bc25cca62b667317
Jason Toris <jason.toris@gmail.com>
2017-09-05 20:21:42 -0400

    Update to support newer kernel and add more disk space

--- script/kernel.sh
+++ script/kernel.sh
@@ -4,7 +4,7 @@ if [[ $UPDATE  =~ true || $UPDATE =~ 1 || $UPDATE =~ yes ]]; then
     # Configure the elrepo repository so we can upgrade the kernel to 4.1
     echo "==> Configuring elrepo repository"
     rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-    yum -y install http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
+    yum -y install http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
 
     echo "==> Installing kernel-ml from elrepo"
     yum -y --enablerepo=elrepo-kernel install kernel-ml
